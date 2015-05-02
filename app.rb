acef6eb4493a2a1211880fb98148ad3d99e0a88e
Pablo Acuña <pabloacuna88@gmail.com>
2015-05-02 18:47:41 -0300

    ADD: use pgsql for data persisting

--- app.rb
+++ app.rb
@@ -4,6 +4,23 @@ class App < Sinatra::Base
                       api_key: ENV['API_KEY'],
                       access_token: ENV['ACCESS_TOKEN']}
 
+  Haml::Options.defaults[:format] = :html5
+
+  DB = Sequel.connect('postgres://pacuna@localhost:5432/postgres')
+
+  DB.create_table? :prizes do
+    primary_key :id
+    varchar :link
+    varchar :name
+    timestamp :created_at
+    varchar :email
+    varchar :location
+    varchar :message
+  end
+
+
+  class Prize < Sequel::Model; end
+
   helpers do
     def h(text)
       Rack::Utils.escape_html(text)
@@ -13,15 +30,21 @@ class App < Sinatra::Base
   get '/' do
     if params[:url]
       thread = Thread.new('archdailycom', params[:url])
-
       winner_index = rand(thread.posts_count)
       @winner_post = thread.posts[winner_index]
       response = Geocoder.search("#{@winner_post["approxLoc"]["lat"]},#{@winner_post["approxLoc"]["lng"]}")
-
       @formatted_address = response.first.data["formatted_address"] if response
     end
 
+    puts Prize.all.inspect
     haml :index, :format => :html5
+  end
+
+  post '/' do
+    prizes = DB[:prizes]
+    prizes.insert(link: params[:link], name: params[:name], created_at: params[:created_at], email: params[:email] , location: params[:location], message: params[:message])
+    puts 'post enviado'
+    redirect to('/')
   end
 
   class Thread
